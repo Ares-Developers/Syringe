c0d0f8b8949a76162161d3d7fd2d0806f5862319
AlexB <Alexander-B@gmx.de>
2015-07-03 17:21:09 +0200

    Added some SAL information to please the analyzer.

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@
 
 auto const VersionString = "Syringe 0.7.0.5";
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hInstance);
 	UNREFERENCED_PARAMETER(hPrevInstance);
