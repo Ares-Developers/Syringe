21121d15fd22fae4c00f70b5d8ff1c14a4cb60d6
AlexB <Alexander-B@gmx.de>
2015-11-15 20:05:18 +0100

    Several changes to Main
    - changed the text referring to Syringe control files
    - renamed a few variables, made them const
    - simplified a few things

--- Main.cpp
+++ Main.cpp
@@ -3,9 +3,9 @@
 
 #include <string>
 
-auto const VersionString = "Syringe 0.7.0.5";
-
 int Run(char* const lpCmdLine) {
+	constexpr auto const VersionString = "Syringe 0.7.0.5";
+
 	Log::Open("syringe.log");
 
 	Log::WriteLine(VersionString);
@@ -13,62 +13,58 @@ int Run(char* const lpCmdLine) {
 	Log::WriteLine();
 	Log::WriteLine("WinMain: lpCmdLine = \"%s\"", lpCmdLine);
 
-	if(lpCmdLine)
+	if(lpCmdLine && *lpCmdLine == '\"')
 	{
-		if(strstr(lpCmdLine, "\"") == lpCmdLine)
+		auto const pFilenameBegin = lpCmdLine + 1;
+		
+		if(auto const pFilenameEnd = strstr(pFilenameBegin, "\""))
 		{
-			char* fn = lpCmdLine + 1;
-			char* args = strstr(fn, "\"");
-
-			if(args)
+			std::string file(pFilenameBegin, pFilenameEnd);
+			
+			Log::WriteLine("WinMain: Trying to load executable file \"%s\"...", file.c_str());
+			Log::WriteLine();
+			SyringeDebugger Debugger;
+			if(Debugger.RetrieveInfo(file))
 			{
-				std::string file(fn, static_cast<unsigned int>(args - fn));
-				++args;
-
-				Log::WriteLine("WinMain: Trying to load executable file \"%s\"...", file.c_str());
+				Log::WriteLine("WinMain: SyringeDebugger::FindDLLs();");
 				Log::WriteLine();
-				SyringeDebugger Debugger;
-				if(Debugger.RetrieveInfo(file))
-				{
-					Log::WriteLine("WinMain: SyringeDebugger::FindDLLs();");
-					Log::WriteLine();
-					Debugger.FindDLLs();
+				Debugger.FindDLLs();
 
-					Log::WriteLine("WinMain: SyringeDebugger::Run(\"%s\");", args);
-					Log::WriteLine();
-					Debugger.Run(args);
+				auto const pArgs = strpbrk(pFilenameEnd + 1, " ");
+				Log::WriteLine("WinMain: SyringeDebugger::Run(\"%s\");", pArgs);
+				Log::WriteLine();
+				Debugger.Run(pArgs);
 
-					Log::WriteLine("WinMain: SyringeDebugger::Run finished.", args);
-					Log::WriteLine("WinMain: Exiting on success.");
-					return 0;
-				}
-				else
-				{
-					char msg[0x280];
-					sprintf_s(msg, "Fatal Error:\r\nCould not load executable file: \"%s\"", file.c_str());
-
-					MessageBoxA(nullptr, msg, "Syringe", MB_OK | MB_ICONERROR);
-
-					Log::WriteLine("WinMain: ERROR: Could not load executable file, exiting...");
-				}
+				Log::WriteLine("WinMain: SyringeDebugger::Run finished.", pArgs);
+				Log::WriteLine("WinMain: Exiting on success.");
+				return 0;
 			}
 			else
 			{
 				char msg[0x280];
-				sprintf_s(msg, "Fatal Error:\r\nCould not evaluate command line arguments: \"%s\"", lpCmdLine);
+				sprintf_s(msg, "Fatal Error:\r\nCould not load executable file: \"%s\"", file.c_str());
 
-				MessageBoxA(nullptr, msg, "Syringe", MB_OK | MB_ICONERROR);
+				MessageBoxA(nullptr, msg, VersionString, MB_OK | MB_ICONERROR);
 
-				Log::WriteLine("WinMain: ERROR: Command line arguments could not be evaluated, exiting...");
+				Log::WriteLine("WinMain: ERROR: Could not load executable file, exiting...");
 			}
 		}
 		else
 		{
-			MessageBoxA(nullptr, "Syringe cannot be run just like that.\r\nPlease run a Syringe control file!",
-				VersionString, MB_OK | MB_ICONINFORMATION);
+			char msg[0x280];
+			sprintf_s(msg, "Fatal Error:\r\nCould not evaluate command line arguments: \"%s\"", lpCmdLine);
 
-			Log::WriteLine("WinMain: ERROR: No command line arguments given, exiting...");
+			MessageBoxA(nullptr, msg, VersionString, MB_OK | MB_ICONERROR);
+
+			Log::WriteLine("WinMain: ERROR: Command line arguments could not be evaluated, exiting...");
 		}
+	}
+	else
+	{
+		MessageBoxA(nullptr, "Syringe cannot be run just like that.\n\nUsage:\nSyringe.exe \"<exe name>\" <arguments>",
+			VersionString, MB_OK | MB_ICONINFORMATION);
+
+		Log::WriteLine("WinMain: ERROR: No command line arguments given, exiting...");
 	}
 
 	Log::WriteLine("WinMain: Exiting on failure.");
