3137a4d7dc7490fc42ee8af2d291e43d86c956c6
AlexB <Alexander-B@gmx.de>
2016-10-10 02:33:05 +0200

    Use constexpr to generate the CRC32 polynomial table, this way the checks for initialization in the constructor can just go away and the class becomes pretty simple.

--- CRC32.cpp
+++ CRC32.cpp
@@ -1,41 +1,34 @@
 #include "CRC32.h"
 
-unsigned int CRC32::table[256];
-bool CRC32::initialized = false;
+#include <array>
+#include <utility>
 
-CRC32::CRC32() noexcept
-{
-	if(!initialized)
-	{
-		initialize();
-	}
+template <unsigned int C, unsigned int K = 8u>
+struct crc32_polynomial // bit-reverse 0x04C11DB7U;
+	: crc32_polynomial<((C & 1u) ? 0xEDB88320u : 0u) ^ (C >> 1u), K - 1u>
+{ };
 
-	reset();
+template <unsigned int C>
+struct crc32_polynomial<C, 0u>
+	: std::integral_constant<unsigned int, C>
+{ };
+
+template <unsigned int Index>
+constexpr auto crc32_polynomial_v = crc32_polynomial<Index>::value;
+
+template <size_t... Indexes>
+constexpr auto IndexesToPolynomial(std::index_sequence<Indexes...>) {
+	return std::integer_sequence<
+		unsigned int, crc32_polynomial<Indexes>::value...>{};
 }
 
-void CRC32::initialize() noexcept
-{
-	for(unsigned int i = 0; i < 256; ++i)
-	{
-		unsigned int value = i;
-
-		for(int j = 8; j > 0; --j)
-		{
-			if(value & 1)
-			{
-				value = (value >> 1) ^ polynomial;
-			}
-			else
-			{
-				value = (value >> 1);
-			}
-		}
-
-		table[i] = value;
-	}
-
-	initialized = true;
+template <typename T, size_t... Values>
+constexpr auto SequenceToArray(std::integer_sequence<T, Values...>) {
+	return std::array<T const, sizeof...(Values)>{ Values... };
 }
+
+constexpr auto const crc_table = SequenceToArray(
+	IndexesToPolynomial(std::make_index_sequence<256>()));
 
 unsigned int CRC32::compute(void* buffer, long long length) noexcept
 {
@@ -43,7 +36,7 @@ unsigned int CRC32::compute(void* buffer, long long length) noexcept
 	{
 		unsigned char byte = static_cast<unsigned char*>(buffer)[i];
 		unsigned char index = (_value & 0xFF) ^ byte;
-		_value = (_value >> 8) ^ table[index];
+		_value = (_value >> 8) ^ crc_table[index];
 	}
 
 	return ~_value;
--- CRC32.h
+++ CRC32.h
@@ -3,8 +3,6 @@
 class CRC32
 {
 public:
-	CRC32() noexcept;
-
 	unsigned int compute(void* buffer, long long length) noexcept;
 
 	unsigned int value() const noexcept {
@@ -16,12 +14,5 @@ public:
 	}
 
 private:
-	static void initialize() noexcept;
-
-	static bool initialized;
-	static unsigned int table[256];
-
 	unsigned int _value{ 0xFFFFFFFFU };
-
-	static const unsigned int polynomial = 0xEDB88320U; // bit-reverse 0x04C11DB7U;
 };
