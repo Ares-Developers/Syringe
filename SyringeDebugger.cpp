Ares-Developers/Syringe

commit b5ad829f12a6e93dfa25911c556adab97c4efe84

Manage thread HANDLEs using ThreadHandle.
The handles are managed automatically, and the special treatment in the destructor is not necessary and more. This actually changes the behavior: when handling the EXIT_THREAD_DEBUG_EVENT, the call to erase now actually closes the handles. Previously they were never closed.

SyringeDebugger.cpp
@@ -30,9 +30,6 @@ SyringeDebugger::SyringeDebugger()
 
 SyringeDebugger::~SyringeDebugger()
 {
-	for(auto i = threadInfoMap.begin(); i != threadInfoMap.end(); ++i) {
-		CloseHandle(i->second.hThread);
-	}
 }
 
 bool SyringeDebugger::DebugProcess(const char* exeFile, char* params)
@@ -97,7 +94,7 @@ DWORD SyringeDebugger::HandleException(const DEBUG_EVENT& dbgEvent)
 
 	if(exceptCode == EXCEPTION_BREAKPOINT)
 	{
-		HANDLE currentThread = threadInfoMap[dbgEvent.dwThreadId].hThread;
+		HANDLE currentThread = threadInfoMap[dbgEvent.dwThreadId].Thread;
 		CONTEXT context;
 
 		context.ContextFlags = CONTEXT_CONTROL;
@@ -352,7 +349,7 @@ DWORD SyringeDebugger::HandleException(const DEBUG_EVENT& dbgEvent)
 	{
 		PatchMem(threadInfoMap[dbgEvent.dwThreadId].lastBP, (LPVOID)&INT3, 1);
 
-		HANDLE hThread = threadInfoMap[dbgEvent.dwThreadId].hThread;
+		HANDLE hThread = threadInfoMap[dbgEvent.dwThreadId].Thread;
 		CONTEXT context;
 
 		context.ContextFlags = CONTEXT_CONTROL;
@@ -373,7 +370,7 @@ DWORD SyringeDebugger::HandleException(const DEBUG_EVENT& dbgEvent)
 		{
 			//Log::SelWriteLine("SyringeDebugger::HandleException: ACCESS VIOLATION at 0x%08X!",exceptAddr);
 
-			HANDLE currentThread = threadInfoMap[dbgEvent.dwThreadId].hThread;
+			HANDLE currentThread = threadInfoMap[dbgEvent.dwThreadId].Thread;
 			CONTEXT context;
 
 			char buffer[0x20] = "\0";
@@ -560,13 +557,13 @@ bool SyringeDebugger::Run(char* params)
 			pInfo.dwThreadId = dbgEvent.dwProcessId;
 			pInfo.hThread = dbgEvent.u.CreateProcessInfo.hThread;
 			pInfo.dwThreadId = dbgEvent.dwThreadId;
-			threadInfoMap[dbgEvent.dwThreadId].hThread = dbgEvent.u.CreateProcessInfo.hThread;
+			threadInfoMap[dbgEvent.dwThreadId].Thread = ThreadHandle(dbgEvent.u.CreateProcessInfo.hThread);
 			threadInfoMap[dbgEvent.dwThreadId].lastBP = nullptr;
 			CloseHandle(dbgEvent.u.CreateProcessInfo.hFile);
 			break;
 
 		case CREATE_THREAD_DEBUG_EVENT:
-			threadInfoMap[dbgEvent.dwThreadId].hThread = dbgEvent.u.CreateThread.hThread;
+			threadInfoMap[dbgEvent.dwThreadId].Thread = ThreadHandle(dbgEvent.u.CreateThread.hThread);
 			threadInfoMap[dbgEvent.dwThreadId].lastBP = nullptr;
 			break;
 

Handle.h
@@ -2,11 +2,22 @@
 #define FILEHANDLE_H
 
 #include <stdio.h>
+#include <Windows.h>
 
 struct FileHandleDeleter {
 	void operator () (FILE* file) {
 		if(file) {
 			fclose(file);
+		}
+	}
+};
+
+struct ThreadHandleDeleter {
+	typedef HANDLE pointer;
+
+	void operator () (pointer handle) {
+		if(handle) {
+			CloseHandle(handle);
 		}
 	}
 };
@@ -52,5 +63,6 @@ private:
 };
 
 using FileHandle = Handle<FILE*, FileHandleDeleter, nullptr>;
+using ThreadHandle = Handle<HANDLE, ThreadHandleDeleter, nullptr>;
 
 #endif

SyringeDebugger.h
@@ -49,7 +49,7 @@ private:
 	//ThreadInfo
 	struct threadInfo
 	{
-		HANDLE hThread;
+		ThreadHandle Thread;
 		LPVOID lastBP;
 	};
 	std::map<DWORD, threadInfo> threadInfoMap;

commit 8faf04586bde89abd28e0cf350eeae32e912a302

Fixed a regression regarding thread handles
This was introduced with b5ad829f.

Threat handles in the threadInfoMap were closed manually when the SyringeDebugger instance was destroyed. By using the ThreadHandle, this is done automatically, even when handling the EXIT_THREAD_DEBUG_EVENT. It previously wasn't handled there. When calling ContinueDebugEvent after such an event, the handle was closed already.

By releasing ownership from the ThreadHandle, the handle is no longer closed.

SyringeDebugger.cpp
@@ -531,6 +531,7 @@ bool SyringeDebugger::Run(char* params)
 			break;
 
 		case EXIT_THREAD_DEBUG_EVENT:
+			threadInfoMap[dbgEvent.dwThreadId].Thread.release();
 			threadInfoMap.erase(dbgEvent.dwThreadId);
 			break;
 