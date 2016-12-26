87bd8349c754158f135b8de2f743b5557fd49b25
AlexB <Alexander-B@gmx.de>
2016-12-26 21:39:43 +0100

    Reworked FindFile

--- FindFile.h
+++ FindFile.h
@@ -8,37 +8,32 @@ class FindFile {
 public:
 	FindFile() = default;
 
-	explicit FindFile(const char* fileName) noexcept {
-		if(fileName) {
-			this->Handle.reset(FindFirstFile(fileName, &this->Data));
-			this->Valid = (this->Handle != INVALID_HANDLE_VALUE);
-		}
-	}
+	explicit FindFile(const char* fileName) noexcept
+		: Handle(FindFirstFile(fileName, &this->Data))
+	{ }
 
-	explicit operator bool() const {
-		return this->Valid;
+	explicit operator bool() const noexcept {
+		return static_cast<bool>(this->Handle);
 	}
 
 	FindFile& operator ++ () noexcept {
-		if(this->Valid) {
+		if(this->Handle) {
 			if(FindNextFile(this->Handle, &this->Data) == FALSE) {
-				this->Valid = false;
 				this->Handle.clear();
 			}
 		}
 		return *this;
 	}
 
-	const WIN32_FIND_DATA* operator -> () const {
+	const WIN32_FIND_DATA* operator -> () const noexcept {
 		return &this->Data;
 	}
 
-	const WIN32_FIND_DATA& operator * () const {
+	const WIN32_FIND_DATA& operator * () const noexcept {
 		return this->Data;
 	}
 
 private:
 	FindHandle Handle;
-	bool Valid{ false };
 	WIN32_FIND_DATA Data;
 };
